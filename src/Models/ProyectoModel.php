f1cc1152fd64a8d536f9178779e2d68b73249d93
Raul <rauespi10@gmail.com>
2026-04-25 18:31:48 -0400

    modelo terminado

--- src/Models/ProyectoModel.php
+++ src/Models/ProyectoModel.php
@@ -12,5 +12,33 @@ class ProyectoModel
         $db = new Database();
         $this->conection = $db->getConnection();
     }
+
+    public function getAll() // Obtener todo los datos
+    {
+        $query = "SELECT * FROM proyectos ORDER BY id DESC";
+        $stmt = $this->conection->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function create($nombre, $descripcion, $responsable)
+    {
+        $query = "INSERT INTO INFO proyectos(nombre, descripcion, responsable)
+        VALUES (:nombre, :descripcion, :responsable)";
+
+        $stmt = $this->conection->prepare($query);
+        $stmt->bindParam(":nombre", $nombre); 
+        $stmt->bindParam(":descripcion", $descripcion);
+        $stmt->bindParam(":responsable", $responsable); 
+        return $stmt->execute();
+    }
+
+    public function delete($id)
+    {
+       $query = "DELETE FROM proyectos WHERE id = :id";
+       $stmt = $this->conection->prepare($query);
+       $stmt->bindParam(":id", $id);
+       return $stmt->execute();
+    }
 }
 ?>
